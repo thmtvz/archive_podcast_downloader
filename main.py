719c156bdf2f2831667370757a5f58b8399b7b53
Thiago Vaz <thiago@martinsvaz.com>
2022-05-23 07:12:17 -0300

    commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+class Audio:
+    def init():
+        return 0
+
+def main(args):
+    return 0
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+#abrir arquivo xml escolhido
+#parse a arvore do docuemento
+#fazer uma lista com todos os links e documentos pra verificar a integridade
+#cada link é um objeto que contem as informaçoes apropriadas sobre o audio
+#iterar os links e tentar baixar e salvar os itens
+#depois de x tentativas desistir de baixar o item com erro
+#reportar os status
